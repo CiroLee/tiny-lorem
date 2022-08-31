ede366130be3a3f6911a8b0d7e5f6e6345f1dd3f
CiroLee <ciromail@126.com>
2022-08-31 20:46:38 +0800

    feat: update test

--- test/utils.spec.ts
+++ test/utils.spec.ts
@@ -4,10 +4,6 @@ describe('utils', () => {
   test('fillDecimal', () => {
     expect(utils.fillDecimal(1, 4)).toBe('1.0000');
   });
-  test('dateFormat, use default params', () => {
-    const result = utils.dateFormat(1660660487000);
-    expect(result).toBe('2022-08-16 22:34:47');
-  });
   test('dateFormat, use default params, format is yyyy/mm/dd', () => {
     const result = utils.dateFormat(1660660487000, 'yyyy/mm/dd');
     expect(result).toBe('2022/08/16');
